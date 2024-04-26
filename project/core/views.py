7dee3cfa6679f64d64ccdf774278ab828db5e786
Nicolás Ferrand <nico_ferrand@hotmail.com>
2024-04-26 17:31:12 -0300

    Cuarto commit

--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
 from . import models
+from cliente import models
+
 
 def home(request):
-    return render(request, "core/index.html")
+    consulta = request.GET.get("consulta", None)
+    if consulta:
+        query = models.Cliente.objects.filter(apellido__icontains=consulta)
+    else:
+        query =  models.Cliente.objects.all()
+    
+    context = {"clientes": query}
+    return render(request, "core/index.html", context)
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render, redirect
 from . import models, forms
 
-
-# def home(request):
-#     consulta = request.GET.get("consulta", None)
-#     if consulta:
-#         print(consulta)
-#         query = models.Producto.objects.filter(nombre__icontains=consulta)
-#     else:
-#         query =  models.Producto.objects.all()
-#     context = {"productos": query}
-#     return render(request, "producto/index.html", context)
-
 def home(request):
     if request.method == "POST":
         form = forms.ProductoCategoriaForms(request.POST)
